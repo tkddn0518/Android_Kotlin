8ae2e24ebf8fbb6038485a47a6e5a7d98f76591b
tkddn0518 <tkddn0518@naver.com>
2022-11-09 16:10:01 +0900

    로그인, 회원가입 기능

--- app/src/main/java/com/example/firebaseexample/LoginActivity.kt
+++ app/src/main/java/com/example/firebaseexample/LoginActivity.kt
@@ -1,16 +1,57 @@
 package com.example.firebaseexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebaseexample.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
+        // Login Button
+        binding.btnLogin.setOnClickListener{
+            val email = binding.loginID.text.toString()
+            val password = binding.loginPassword.text.toString()
+            logIn(email, password)
+        }
+
+    }
+
+    // LogIn function
+    private fun logIn(id: String, password: String) {
+        if (id.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(id, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "로그인 성공", Toast.LENGTH_LONG).show()
+                        goToMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(baseContext, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    // 유저 정보 확인 후 메인 페이지로 이동하는 함수
+    private fun goToMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/firebaseexample/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseexample/RegisterActivity.kt
@@ -2,15 +2,46 @@ package com.example.firebaseexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.firebaseexample.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
 
+    // FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
+        // CreateAccount Button
+        binding.btnCreateAccount.setOnClickListener {
+            val email = binding.registerID.text.toString()
+            val password = binding.registerPassword.text.toString()
+
+            createAccount(email, password)
+        }
+    }
+
+    // 계정 생성 함수
+    private fun createAccount(id: String, password: String) {
+        if (id.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(id, password)
+                ?.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "계정 생성 완료", Toast.LENGTH_LONG).show()
+                        finish()
+                    } else {
+                        Toast.makeText(this, "계정 생성 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
 }
