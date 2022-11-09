0fdbcd96b6f44f2be3a54f596158d5a2eb32ce63
tkddn0518 <tkddn0518@naver.com>
2022-11-09 16:06:01 +0900

    viewbinding 설정

--- app/src/main/java/com/example/firebaseexample/RegisterActivity.kt
+++ app/src/main/java/com/example/firebaseexample/RegisterActivity.kt
@@ -2,10 +2,15 @@ package com.example.firebaseexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaseexample.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
